ecdeebb387237034f29b4dc195fa5bf26e6c18f5
Alister Cedeno <cranalister@gmail.com>
2018-09-19 19:36:56 -0700

    Car Insurance

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string age = Console.ReadLine();
+            int age1 = Convert.ToInt32(age);
+            Console.WriteLine("Have you ever had a DUI? Answer Yes or No:");
+            string DUI = Console.ReadLine();
+            bool DUIB = (DUI == "No"|| DUI == "no");
+            Console.WriteLine("How many speeding tickets do you have?");
+            string ticket = Console.ReadLine();
+            int ticket1 = Convert.ToInt32(ticket);
+            Console.WriteLine("Qualified?");
+            bool results = (age1 > 15 && ticket1 <= 3);
+            bool results1 = (DUIB && results);
+            Console.WriteLine(results1);
+            Console.ReadLine();
+
+
+        }
+    }
+}
