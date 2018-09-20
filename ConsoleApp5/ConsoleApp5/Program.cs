36962f744344519e8730a4dc07d6a7627ff1a1a7
Alister Cedeno <cranalister@gmail.com>
2018-09-19 20:26:38 -0700

    Shipping Package

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+            Console.WriteLine("Please supply a accurate weight for the package you wish to ship:");
+            double packageWeight = Convert.ToInt32(Console.ReadLine());
+
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day. ");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("Please supply a accurate Width for the package");
+            float packageWidth = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please supply a accurate Height for the package");
+            float packageHeight = float.Parse(Console.ReadLine());
+            Console.WriteLine("Please supply a accurate Lenth for the package");
+            float packageLength = float.Parse(Console.ReadLine());
+
+            if (packageWidth + packageHeight + packageLength > 50)
+            {
+                Console.WriteLine("Package too big to be shipped via Package Express");
+                Console.ReadLine();
+                return;
+            }
+
+            double results = (packageWidth + packageHeight + packageLength);
+            double result1= (((results) * packageWeight) / 100);
+            Console.WriteLine("Your estimated total for shipping this package is: $" + result1 + " Thank You.");
+            Console.ReadLine();
+
+        }
+    }
+}
