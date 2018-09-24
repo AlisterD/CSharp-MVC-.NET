b7b055bda34d125c16f2dbf69c68355ebe280b20
Alister Cedeno <cranalister@gmail.com>
2018-09-24 15:25:25 -0700

    RecallingClassesandAssigningValues

---
+++ ConsoleApp13/ConsoleApp13/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Class1
+    {
+        public int method(int x)
+        {
+            return x + 10;
+            
+        }
+
+        public int method(decimal y)
+        {
+            return Convert.ToInt32(y / 7);
+            
+        }
+        public int method(string s)
+        {
+
+            int b = Convert.ToInt32(s) + 20;
+            return b;
+
+          
+        }
+
+
+    }
+
+}
---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //In the Main() method of the console app, instantiate the class and call the one method, passing in an integer, such as 12. Then display the result to the screen.
+
+            Class1 method = new Class1();
+            Console.WriteLine(method.method(12));
+
+
+
+            //In the Main() method of the console app, instantiate the class and call the second method, passing in a decimal. Display the result to the screen.
+
+            Console.WriteLine(method.method(9));
+
+
+            //In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer.Display the result to the screen.
+
+            Console.WriteLine(method.method(100));
+            Console.ReadLine();
+        }
+    }
+}
