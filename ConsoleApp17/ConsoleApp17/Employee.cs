8b2a1e091128b01ae8c035eb4bbb3dcee61de9ee
Alister Cedeno <cranalister@gmail.com>
2018-09-25 15:02:33 -0700

    Interface and Inheritance Sample

---
+++ ConsoleApp17/ConsoleApp17/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Employee : Person, IQuittable 
+    {
+        int Id = 100;
+
+        public Employee(string FirstName, string LastName) : base(FirstName, LastName)
+        {
+        }
+        public void Quit()
+        {
+            Console.WriteLine("If you press Enter, you will Exit");
+            Console.ReadLine();
+            Environment.Exit(0);
+
+        }
+
+        public override void NameofEmployee()
+        {
+            Console.WriteLine("Name:" + base.FirstName + base.LastName);
+
+        }
+    }
+
+    
+}
---
+++ Sample TwentyOne/Sample TwentyOne/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample_TwentyOne
+{
+        public class Player
+        {
+            public List<Class> Hand { get; set; }
+            public int Balance { get; set; }
+            public string Name { get; set; }
+            public bool isActivelyPlaying { get; set; }
+        }
+    
+}
