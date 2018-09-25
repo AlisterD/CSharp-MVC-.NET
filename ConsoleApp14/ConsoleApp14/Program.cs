3d0aea07f40e6b10eb4559ef7346e5700a851e0c
Alister Cedeno <cranalister@gmail.com>
2018-09-24 18:40:48 -0700

    ObjectandClass

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass myObject = new myClass();
+            Console.WriteLine("Please enter a number. The Second inout may be let blank: ");
+           string first = Console.ReadLine();
+           string second = Console.ReadLine();
+
+            int total;
+            if (string.IsNullOrWhiteSpace(second))
+            total = myObject.myMethod(Convert.ToInt32(first));
+            else
+                total = myObject.myMethod(Convert.ToInt32(first) , Convert.ToInt32(second));
+            Console.WriteLine("The returned value of both of your numbers is: " + total + "."); 
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ConsoleApp14/ConsoleApp14/myClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class myClass
+    {
+  
+        public int myMethod(int x, int y = 10)
+        {
+          return x + y;  
+        }
+
+    }
+}
