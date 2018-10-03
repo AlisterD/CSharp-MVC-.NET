1f762d0b46da24533fc0039335c5e9bfff9e77b2
Alister Cedeno <cranalister@gmail.com>
2018-10-03 15:31:05 -0700

    DateTime sample

---
+++ ConsoleApp24/ConsoleApp24/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please type a number:");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You entered {0} on:", answer);
+            Console.WriteLine(DateTime.Now);
+            DateTime now = DateTime.Now;
+            DateTime later = now.AddHours(answer);
+            Console.WriteLine("Time in {0} hours: {1}", answer, later.TimeOfDay);
+            Console.ReadLine();
+
+
+        }
+    }
+}
